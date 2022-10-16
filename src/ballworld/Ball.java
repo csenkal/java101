8ec74fc7f59f36ccc4c2a47340b94026526e6338
csenkal <cagrisenkal@hotmail.com>
2022-10-16 16:02:30 +0300

    Ballworld first commit

---
+++ src/ballworld/Ball.java
@@ -0,0 +1,71 @@
+package ballworld;
+
+//
+//	general purpose reusable bouncing ball abstraction
+//	Described in Chapter 5 of
+//	Understanding Object-Oriented Programming with Java
+//	by Timothy A Budd
+//	Published by Addison-Wesley
+//
+//	see ftp://ftp.cs.orst.edu/pub/budd/java/ReadMe.html
+//	for further information
+//
+
+import java.awt.*;
+
+public class Ball {
+    protected Rectangle location;
+    protected double dx;
+    protected double dy;
+    protected Color color;
+
+    public Ball (int x, int y, int r)
+    {
+        location = new Rectangle(x-r, y-r, 2*r, 2*r);
+        dx = 0;
+        dy = 0;
+        color = Color.blue;
+    }
+
+    // functions that set attributes
+    public void setColor (Color newColor)
+    { color = newColor;	}
+
+    public void setMotion (double ndx, double ndy)
+    { dx = ndx; dy = ndy; }
+
+    // functions that access attributes of ball
+    public int radius ()
+    { return location.width / 2; }
+
+    public int x ()
+    { return location.x + radius(); }
+
+    public int y ()
+    { return location.y + radius(); }
+
+    public double xMotion ()
+    { return dx; }
+
+    public double yMotion ()
+    { return dy; }
+
+    public Rectangle region () { return location; }
+
+    // functions that change attributes of ball
+    public void moveTo (int x, int y)
+    { location.setLocation(x, y); }
+
+    public void move ()
+    {
+        location.translate ((int) dx, (int) dy);
+
+
+    }
+
+    public void paint (Graphics g)
+    {
+        g.setColor (color);
+        g.fillOval (location.x, location.y, location.width, location.height);
+    }
+}
---
+++ src/ballworld/BallWorld.java
@@ -0,0 +1,83 @@
+package ballworld;
+
+//
+//	the Ball World game
+//	Described in Chapter 5 of
+//	Understanding Object-Oriented Programming with Java
+//	by Timothy A Budd
+//	Published by Addison-Wesley
+//
+//	see ftp://ftp.cs.orst.edu/pub/budd/java/ReadMe.html
+//	for further information
+//
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class BallWorld extends JFrame {
+
+    public static void main (String [ ] args)
+    {
+        BallWorld world = new BallWorld (Color.red);
+        world.setVisible (true);
+    }
+
+    private static final int FrameWidth = 600;
+    private static final int FrameHeight = 400;
+    private Ball aBall;
+
+    private JPanel mainPanel;
+
+
+    private int width;
+    private int height;
+    private BallWorld (Color ballColor) {
+        // constructor for new ball world
+        // resize our frame
+        //setSize (FrameWidth, FrameHeight);
+        setTitle ("Ball World");
+
+        mainPanel = new JPanel(){
+            @Override
+            protected void paintComponent(Graphics g) {
+               //super.paintComponent(g);
+                Graphics2D g2d = (Graphics2D) g;
+                g2d.setStroke(new BasicStroke(4, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+                g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                g.clearRect(0,0,width,height);
+                aBall.paint (g);
+
+                if ((aBall.x() < 0) || (aBall.x() > width))
+                    aBall.setMotion (-aBall.xMotion(), aBall.yMotion());
+                if ((aBall.y() < 0) || (aBall.y() > height))
+                    aBall.setMotion (aBall.xMotion(), -aBall.yMotion());
+            }
+        };
+        mainPanel.setPreferredSize(new Dimension(FrameWidth,FrameHeight));
+        this.add(mainPanel);
+        this.pack();
+        // initialize object data field
+        aBall = new Ball (10, 15, 20);
+        aBall.setColor (ballColor);
+        aBall.setMotion (1.0, 1.0);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        Timer timer = new Timer(100, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                width = mainPanel.getWidth();
+                height = mainPanel.getHeight();
+                aBall.move();
+                mainPanel.repaint();
+                //repaint();
+            }
+        });
+        timer.start();
+    }
+
+}
