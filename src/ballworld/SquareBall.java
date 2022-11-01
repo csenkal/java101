a87c075e62c98250ac0ba1cf6737ecad2471a43f
dozkan01 <denizkozkan@hotmail.com>
2022-11-01 16:27:56 +0300

    comment ekledim

---
+++ src/ballworld/SquareBall.java
@@ -0,0 +1,16 @@
+package ballworld;
+
+import java.awt.*;
+
+public class SquareBall extends Ball {
+
+    public SquareBall(int x, int y, int r) {
+        super(x, y, r);
+    }
+
+    public void paint (Graphics g)
+    {
+        g.setColor (color);
+        g.fillRect (location.x, location.y, location.width, location.height);
+    }
+}
